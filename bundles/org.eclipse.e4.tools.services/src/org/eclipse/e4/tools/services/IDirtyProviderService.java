2dfa7d774fd4f5a0cb115de69fa769c61db73a7a
cvs2svn <platform-releng-dev@eclipse.org>
2010-06-30 10:35:56 +0000

    This commit was manufactured by cvs2svn to create branch 'intermediate_rc2'.
    Sprout from master 2010-06-30 10:35:54 UTC Lars Vogel <lvogel> 'Change shortcut for Toms live editor to ALT+SHIFT+F9'
    Delete:
        bundles/org.eclipse.e4.tools.context/.classpath
        bundles/org.eclipse.e4.tools.context/.project
        bundles/org.eclipse.e4.tools.context/.settings/org.eclipse.jdt.core.prefs
        bundles/org.eclipse.e4.tools.context/.settings/org.eclipse.pde.core.prefs
        bundles/org.eclipse.e4.tools.context/META-INF/MANIFEST.MF
        bundles/org.eclipse.e4.tools.context/build.properties
        bundles/org.eclipse.e4.tools.context/plugin.xml
        bundles/org.eclipse.e4.tools.context/src/org/eclipse/e4/tools/handlers/DiffSnapshotHandler.java
        bundles/org.eclipse.e4.tools.context/src/org/eclipse/e4/tools/handlers/GenerateSnapshotHandler.java
        bundles/org.eclipse.e4.tools.jdt.templates/.classpath
        bundles/org.eclipse.e4.tools.jdt.templates/.project
        bundles/org.eclipse.e4.tools.jdt.templates/.settings/org.eclipse.jdt.core.prefs
        bundles/org.eclipse.e4.tools.jdt.templates/META-INF/MANIFEST.MF
        bundles/org.eclipse.e4.tools.jdt.templates/build.properties
        bundles/org.eclipse.e4.tools.jdt.templates/icons/full/elcl16/e4assist_co.gif
        bundles/org.eclipse.e4.tools.jdt.templates/plugin.xml
        bundles/org.eclipse.e4.tools.jdt.templates/src/org/eclipse/e4/internal/tools/jdt/templates/E4ContextType.java
        bundles/org.eclipse.e4.tools.jdt.templates/src/org/eclipse/e4/internal/tools/jdt/templates/E4TemplateCompletionProposalComputer.java
        bundles/org.eclipse.e4.tools.jdt.templates/templates/default-e4templates.properties
        bundles/org.eclipse.e4.tools.jdt.templates/templates/default-e4templates.xml
        bundles/org.eclipse.e4.tools.services/src/org/eclipse/e4/tools/services/IDirtyProviderService.java
        bundles/org.eclipse.e4.tools.services/src/org/eclipse/e4/tools/services/ISelectionProviderService.java
        features/org.eclipse.e4.core.tools.feature/.project
        features/org.eclipse.e4.core.tools.feature/build.properties
        features/org.eclipse.e4.core.tools.feature/feature.properties
        features/org.eclipse.e4.core.tools.feature/feature.xml
        features/org.eclipse.e4.core.tools.feature/sourceTemplateFeature/eclipse_update_120.jpg
        features/org.eclipse.e4.core.tools.feature/sourceTemplateFeature/epl-v10.html
        features/org.eclipse.e4.core.tools.feature/sourceTemplateFeature/feature.properties
        features/org.eclipse.e4.core.tools.feature/sourceTemplateFeature/license.html
        features/org.eclipse.e4.core.tools.feature/sourceTemplatePlugin/build.properties

--- bundles/org.eclipse.e4.tools.services/src/org/eclipse/e4/tools/services/IDirtyProviderService.java
+++
@@ -1,5 +0,0 @@
-package org.eclipse.e4.tools.services;
-
-public interface IDirtyProviderService {
-	public void setDirtyState(boolean dirtyState);
-}
--- bundles/org.eclipse.e4.tools.services/src/org/eclipse/e4/tools/services/ISelectionProviderService.java
+++
@@ -1,5 +0,0 @@
-package org.eclipse.e4.tools.services;
-
-public interface ISelectionProviderService {
-	public void setSelection(Object selection);
-}
